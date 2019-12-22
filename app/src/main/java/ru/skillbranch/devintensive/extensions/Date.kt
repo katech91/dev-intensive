b77b8a535520e5f231cfa4fecb29b71fbf575fb2
Katerina Chernysheva <katech91@gmail.com>
2019-12-22 21:49:14 +0300

    Fix humanize date

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -59,6 +59,15 @@ fun Date.humanizeDiff(date: Date = Date()): String {
         in (75*MINUTE)..(22* HOUR) -> humanReadableDiff = "${(diffSec/ HOUR).toInt()} ${TimeUnits.HOUR.plural((diffSec/ HOUR).toInt())} назад"
         in (22* HOUR)..(26* HOUR) -> humanReadableDiff = "день назад"
         in (26* HOUR)..(360* DAY ) -> humanReadableDiff = "${(diffSec/ DAY).toInt()} ${TimeUnits.DAY.plural((diffSec/ DAY).toInt())} назад"
+        in -44999..-1000 -> humanReadableDiff = "через несколько секунд"
+        in -74999..-45000 -> humanReadableDiff = "через минуту"
+        in (-45*MINUTE)..-75000 -> humanReadableDiff = "через ${(diffSec/MINUTE).toInt()} ${TimeUnits.MINUTE.plural((diffSec/MINUTE).toInt())}"
+        in (-75*MINUTE)..(-45* MINUTE) -> humanReadableDiff = "через час"
+        in (-22* HOUR)..(-75*MINUTE) -> humanReadableDiff = "через ${(diffSec/ HOUR).toInt()} ${TimeUnits.HOUR.plural((diffSec/ HOUR).toInt())}"
+        in (-26* HOUR)..(-22* HOUR) -> humanReadableDiff = "через день"
+        in (-360* DAY )..(-26* HOUR) -> humanReadableDiff = "через ${(diffSec/ DAY).toInt()} ${TimeUnits.DAY.plural((diffSec/ DAY).toInt())}"
+        in Long.MIN_VALUE..(-360* DAY) -> humanReadableDiff = "через ${(diffSec/ DAY).toInt()} ${TimeUnits.DAY.plural((diffSec/ DAY).toInt())}"
+
         else -> humanReadableDiff = "более года назад"
     }
 
